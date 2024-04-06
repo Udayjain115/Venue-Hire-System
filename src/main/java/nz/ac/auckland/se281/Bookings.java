507c224a448eb5f68ae133f44612fb98743a1bda
Uday <udayrajveerjain@gmail.com>
2024-04-06 19:32:50 +1300

    Added Class for bookings, currently does not extend Venue, might change this later

---
+++ src/main/java/nz/ac/auckland/se281/Bookings.java
@@ -0,0 +1,29 @@
+package nz.ac.auckland.se281;
+
+public class Bookings {
+  private String venueCode;
+  private String Date;
+  private String customerEmail;
+  private String Capacity;
+  private String bookingReference;
+  private String venueName;
+
+  public Bookings(
+      String venueCode,
+      String Date,
+      String customerEmail,
+      String Capacity,
+      String bookingReference,
+      String venueName) {
+    this.venueCode = venueCode;
+    this.Date = Date;
+    this.customerEmail = customerEmail;
+    this.Capacity = Capacity;
+    this.bookingReference = bookingReference;
+    this.venueName = venueName;
+  }
+
+  public String getDate() {
+    return Date;
+  }
+}
